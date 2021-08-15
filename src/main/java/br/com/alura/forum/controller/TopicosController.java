abad2c845446756cb2c5c75f4a1221919aebe416
Rodrigo Oliveira de Almeida <rdg.oliveiraa@gmail.com>
2021-08-15 08:38:08 -0300

    Aula-1 Alteracao da anotacao @controller para @RestController para remocao de necessidade de repeticoes da anotacao @ResponseBody em cada metodo, criacao de classe TopicoDto para devolucao de json

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+public class TopicosController {
+
+    @RequestMapping("/topicos")
+    @ResponseBody
+    public List<TopicoDto> lista() {
+        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programacao"));
+
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    }
+}
