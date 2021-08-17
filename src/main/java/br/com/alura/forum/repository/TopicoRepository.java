54b69ad0e03b45dc01fdd99016f7c953f6d2c366
Rodrigo Oliveira de Almeida <rdg.oliveiraa@gmail.com>
2021-08-16 23:19:54 -0300

    Aula-4 Criacao de metodo POST

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,7 +2,6 @@ package br.com.alura.forum.repository;
 
 import br.com.alura.forum.modelo.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
